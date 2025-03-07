505983fba9964e70472317ea0647ef1366b4c789
Saurabh Bhimrao Patil <122868065+iamsaurabhpatil7381@users.noreply.github.com>
2025-03-07 19:42:48 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,12 +1,12 @@
 class Hello{
     public static void main(String args[])
   {
-      System.out.println(hy("hello my friends");
+      hy("hello my friends");
   }
 
 static void  hy(String str)
   {
-    System.out.println(str):
+    System.out.println(str);
   }
 }
   
